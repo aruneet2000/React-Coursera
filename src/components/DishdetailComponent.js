fe0a8fed1cba520ac8ab908c7a99aa5b371efbe4
Aruneet Roychowdhury <aruneet2015@gmail.com>
2020-07-23 21:49:26 +0530

    Functional Components, Header and Footer, React Router

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
 
-class Dishdetail extends Component {
 
-    renderComments(comments) {
+
+    function RenderComments({ comments }) {
         if (comments == null) {
             return (<div></div>)
         }
@@ -17,7 +17,7 @@ class Dishdetail extends Component {
                             year: 'numeric',
                             month: 'long',
                             day: '2-digit'
-                        }).format(new Date(Date.parse(comment.date)))}
+                        }).format(new Date(comment.date))}
                     </p>
                 </li>
             )
@@ -33,7 +33,7 @@ class Dishdetail extends Component {
         )
     }
 
-    renderDish(dish) {
+    function RenderDish({ dish }) {
         if (dish != null) {
             return (
                 <div className='col-12 col-md-5 m-1'>
@@ -52,22 +52,20 @@ class Dishdetail extends Component {
         }
     }
 
-    render() {
-        const dish = this.props.dish
+    const dishDetail = (props) => {
+        const dish = props.dish
         if (dish == null) {
             return (<div></div>)
         }
-        const dishItem = this.renderDish(dish)
-        const commentItem = this.renderComments(dish.comments)
+        const dishItem = <RenderDish dish = {props.dish}/>
+        const commentItem = <RenderComments comments = {props.dish.comments} />
         return (
-            <div class = "container">
             <div className='row'>
                 {dishItem}
                 {commentItem}
             </div>
-            </div>
         )
     }
-}
 
-export default Dishdetail
+
+export default dishDetail;
