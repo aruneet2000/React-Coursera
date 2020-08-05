b31e113f4239451773960a3bdd0836e69e36a0b7
Aruneet Roychowdhury <aruneet2015@gmail.com>
2020-08-05 11:53:16 +0530

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import { Card, CardImg, CardImgOverlay, CardText,
       Button, Modal, ModalHeader, ModalBody, Row, Col, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 
 function RenderComments({comments, addComment, dishId}) {
@@ -57,8 +58,23 @@ function RenderComments({comments, addComment, dishId}) {
 
     const dishDetail = (props) => {
         const dish = props.dish
-        if (dish == null) {
-            return (<div></div>)
+        if (props.isLoading) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.errMess) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
         }
         const dishItem = <RenderDish dish = {props.dish}/>
         const commentItem = <RenderComments comments = {props.dish.comments} />
