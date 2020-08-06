f54aac957d97f92b35bf3017a0224261158f9ac8
Aruneet Roychowdhury <aruneet2015@gmail.com>
2020-08-06 11:59:50 +0530

    Fetch Post Comment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -7,7 +7,7 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
 
-function RenderComments({comments, addComment, dishId}) {
+function RenderComments({comments, postComment, dishId}) {
         if (comments == null) {
             return (<div></div>)
         }
@@ -32,7 +32,7 @@ function RenderComments({comments, addComment, dishId}) {
                 <ul className='list-unstyled'>
                     {cmnts}
                 </ul>
-                <CommentForm dishId={dishId} addComment={addComment} />
+                <CommentForm dishId={dishId} postComment={postComment} />
             </div>
         )
     }
@@ -95,7 +95,7 @@ function RenderComments({comments, addComment, dishId}) {
                     
                         <RenderDish dish={props.dish} />
                         <RenderComments comments={props.comments}
-                                       addComment={props.addComment}
+                                       postComment={props.postComment}
                                        dishId={props.dish.id}
                         />
                 </div>
@@ -128,7 +128,7 @@ export class CommentForm extends Component {
 
     handleSubmit(values){
         this.toggleModal();
-        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+        this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     render() {
